ff58dc07eedb6449a43ad5efee3ff16c6e8550f7
huynhnhathao <18520714@gm.uit.edu.vn>
2021-11-10 15:40:38 +0700

    working on crawl data

---
+++ crawl_data/crawl_data/spiders/companies_spider.py
@@ -0,0 +1,18 @@
+import scrapy
+
+
+class CompaniesSpider(scrapy.Spider):
+    name = "Compannies"
+    start_urls = [
+        'https://itviec.com/companies',
+    ]
+
+    def parse(self, response):
+        all_companies = response.xpath("//div[@class='first-group companies']/a[@class='featured-company']/@href").getall()
+        yield all_companies
+
+        next_page = response.xpath("//a[@class='more-jobs-link more-company']/@href").get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+            
