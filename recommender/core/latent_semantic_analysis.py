954b0a76932f00e03ab03353e105d7712f9e91f9
huynhnhathao <18520714@gm.uit.edu.vn>
2021-11-15 19:04:10 +0700

    add some work

--- recommender/core/latent_semantic_analysis.py
+++ recommender/core/latent_semantic_analysis.py
@@ -1,7 +1,3 @@
-"""
-This module implement the LSA class, which will take reponsible for processing
-text and compare two documents using the latent semantic analysis algorithm.
-"""
 from typing import Optional
 import string
 import collections
@@ -9,49 +5,73 @@ import numpy as np
 
 import nltk
 from nltk.corpus import stopwords
+from sklearn.feature_extraction import text 
+from sklearn import decomposition
 
 nltk.download('stopwords')
 nltk.download('wordnet')
 stop_words = set(stopwords.words("english"))
 
-def make_vocab(texts: str, num_vocab: Optional[int] = None, min_word_count: Optional[int] = None) -> None:
-    """Create a vocab mapping {term: index}}
-    Args:
-        texts: one string that contains all the text data
-        num_vocab: the desired number of vocabulary
-        min_word_count: if specified, the number of vocab will be infered such 
-        that every vocabulary appear at least min_word_count times int the texts
+
+class LSA:
+
+    """This class's instance can represent text using latent semantic analysis
+    and compare two text using Cosine similarity.
     """
-
-    if num_vocab is None and min_word_count is None:
-        raise ValueError('Either num_vocab or min_word_count must be provided')
-
-    texts = texts.lower()
-    # remove puntuations
-    texts = texts.translate(str.maketrans('', '', string.punctuation))
     
-    texts = texts.split()
-    # remove stopwords
-    texts = [x for x in texts if x not in stop_words]
-    # lemmatize words
-    lemma = nltk.wordnet.WordNetLemmatizer()
-    texts = [lemma.lemmatize(x) for x in texts]
-    # takes num_vocab most common words
-    counter = collections.Counter(texts)
+    def __init__(self, texts: str, num_vocab: Optional[int] = None,
+                min_word_count: Optional[int] = None,
+                num_features: int = 5000) -> None:
+        """
+        Args:
+            texts: one single string contains all the text data
+            num_vocab: number of desired vocab
+            min_word_count: if specified, every word in vocabulary must has count
+                more than or equal to this number, and num_vocab will be ignored
+            num_features: number of features after dimension reduction
+        """
+        self.num_fe
+        self.vocab = self.make_vocab(texts, num_vocab, min_word_count)
 
-    selected_words = []
-    if min_word_count is None and num_vocab is not None:
-        selected_words = counter.most_common(num_vocab)
-        selected_words = [x[0] for x in selected_words]
-    elif min_word_count is not None:
-        word_count = counter.most_common(len(texts))
-        selected_words = [x[0] for x in word_count if x[1] >= min_word_count]
 
-    
-    vocab = {w: i+1 for i, w in enumerate(selected_words)}
-    vocab['_unknown_'] = 0
-    
-    return vocab
+    def make_vocab(texts: str, num_vocab: Optional[int] = None, min_word_count: Optional[int] = None) -> None:
+        """Create a vocab mapping {term: index}}
+        Args:
+            texts: one string that contains all the text data
+            num_vocab: the desired number of vocabulary
+            min_word_count: if specified, the number of vocab will be infered such 
+            that every vocabulary appear at least min_word_count times int the texts
+        """
+
+        if num_vocab is None and min_word_count is None:
+            raise ValueError('Either num_vocab or min_word_count must be provided')
+
+        texts = texts.lower()
+        # remove puntuations
+        texts = texts.translate(str.maketrans('', '', string.punctuation))
+        
+        texts = texts.split()
+        # remove stopwords
+        texts = [x for x in texts if x not in stop_words]
+        # lemmatize words
+        lemma = nltk.wordnet.WordNetLemmatizer()
+        texts = [lemma.lemmatize(x) for x in texts]
+        # takes num_vocab most common words
+        counter = collections.Counter(texts)
+
+        selected_words = []
+        if min_word_count is None and num_vocab is not None:
+            selected_words = counter.most_common(num_vocab)
+            selected_words = [x[0] for x in selected_words]
+        elif min_word_count is not None:
+            word_count = counter.most_common(len(texts))
+            selected_words = [x[0] for x in word_count if x[1] >= min_word_count]
+
+        
+        vocab = {w: i+1 for i, w in enumerate(selected_words)}
+        vocab['_unknown_'] = 0
+        
+        return vocab
 
 
 if __name__ == '__main__':
---
+++ recommender/core/network_builder.py
@@ -0,0 +1,183 @@
+from typing import Dict, List, Any
+
+import numpy as np
+import pandas as pd
+import networkx as nx
+
+import latent_semantic_analysis
+
+class NetworkBuilder:
+    """This class build a network of jobs, employers and candidates.
+
+    First, all the entities of three types will be added to one single network
+    as nodes. Each node has attribute type: str to tell which type is it.
+
+    Then all the relations that can be extracted directly from data will be added
+    to the networks as edges. Those relations include a company posted a job.
+
+    Then the similar relation between two node of the same type can be computed 
+    using latent semantic analysis, the most k percent similar nodes will be connected
+    by the 'similar' edge.
+
+    The next relation is 'profile match', which is a relation between a candidate 
+    and a job. We can derrive this relation using LSA as above.
+
+    Another interaction relations can be added from data are apply, favorite, 
+    like and visit. We will consider simulate it.
+
+    The network only contains the semantic relationships beween nodes, not the 
+    data of these node. We will use the entity's id to represent an entity.
+
+
+
+    Node types:
+        Employer: str: a unique string id for an employer
+        Candidate: str: a unique string id for a candidate
+        Job: str: a unique string id for a job posted by an employer
+
+    
+    """
+    def __init__(self, employers_data: pd.DataFrame, 
+                jobs_data: pd.DataFrame,
+                cv_data: pd.DataFrame) -> None:
+        """
+        Args:
+            employer_data: a dict map from employer/company unique id to its data
+            job_data: a dict map from job's unique id to its data
+            cv_data: CV of candidates in the network
+
+        """
+        if isinstance(employers_data, pd.DataFrame):
+            self.employers_data = self.employers_dataframe_to_dict(employers_data)
+        else:
+            self.employers_data = employers_data
+
+        if isinstance(jobs_data, pd.DataFrame):
+            self.jobs_data = self.jobs_dataframe_to_dict(jobs_data)
+        else:
+            self.jobs_data = jobs_data
+
+        if isinstance(cv_data, pd.DataFrame):
+            self.cv_data = self.cv_dataframe_to_dict(cv_data)
+        else:
+            self.cv_data = cv_data
+
+        # Create a text comparer using latent_semantic_analysis
+        self.comparer = self.get_lsa()
+
+        # Create the master jobs network
+        self.G = self.create_network_from_data()
+
+    def employers_dataframe_to_dict(self,
+                        companies_df: pd.DataFrame) -> Dict[str, Dict[str, Any]]:
+                        
+        # this method only transform dataframe to dict, it does not add any 
+        # information into the transformed dict
+        employers_data = {}
+        for i, row in companies_df.iterrows():
+            employer_data = {'company_name': row['company_name'],
+                        'average_rating': row['average_rating'],
+                        'num_review': row['num_review'],
+                        'city': row['city'],
+                        'type': row['type'],
+                        'num_employee': row['num_employee'],
+                        'country': row['country'],
+                        'working_day': row['working_day'],
+                        'OT': row['OT'],
+                        'overview': row['overview'],
+                        'expertise': row['expertise'],
+                        'benifit': row['benifit'],
+                        'logo_link': row['logo_link']}
+            
+            employers_data[row['company_id']] = employer_data
+
+        return employers_data
+
+    def jobs_dataframe_to_dict(self,
+                            jobs_df: pd.DataFrame) -> Dict[str, Dict[str, Any]]:
+
+        # this method only transform dataframe to dict, it does not add any 
+        # information into the transformed dict
+        jobs_data = {}
+        for i, row in jobs_df.iterrows():
+            job_data = {'company_id': row['company_id'],
+                    'job_name': row['job_name'],
+                    'taglist': row['taglist'],
+                    'location': row['location'], 
+                    'three_reasons': row['three_reasons'],
+                    'description': row['description']}
+            jobs_data[row['job_id']] = job_data
+                                        
+        return jobs_data
+
+    def cv_dataframe_to_dict(self, 
+                            cv_df: pd.DataFrame) -> Dict[str, Dict[str, Any]]:
+
+        # this method only transform dataframe to dict, it does not add any 
+        # information into the transformed dict
+        cv_data = {}
+        for i, row in cv_df.iterrows():
+            
+            cv_data[i] = {'expertise': row['Category'],
+                        'resume': row['Resume'] }
+
+        return cv_data
+
+    def get_lsa() -> Any:
+        pass
+
+    def create_network_from_data(self, ) -> nx.MultiDiGraph:
+        """
+        Create a networkx MultiDiGraph to represent employers, jobs, candidates
+        as nodes and the 'posted' relations between employer and job as edge.
+
+        The graph created by this method only create edges that can be directly 
+        infered from data, such as 'posted' and interaction edges.
+        Other types of edges that need to compare objects will be added in another
+        method
+        """
+
+        # graph counts and saves its nodes and edges types
+        G = nx.MultiDiGraph(name = 'Jobs graph',
+                            num_employers = 0, 
+                            num_jobs = 0,
+                            num_candidates = 0,
+                            num_candidate_match_job = 0,
+                            num_similar_candidates = 0,
+                            num_similar_jobs = 0,
+                            num_similar_employers = 0,
+                            num_apply = 0,
+                            num_favorite = 0)
+
+        # first add all employer nodes to the network and its data
+        for employer_id, employer_data in self.employers_data.items():
+            G.add_node(employer_id, node_type = 'employer', **employer_data)
+            G.graph['num_employers'] += 1
+            
+        # add all job nodes and the bidirectional edges from job node to employer node
+        for job_id, job_data in self.jobs_data.items():
+            G.add_node(job_id, node_type = 'job', **job_data)
+            G.graph['num_jobs'] += 1
+            # add two edges between job and its employers\
+            G.add_edge(job_id, job_data['company_id'], weight = 1, 
+                        edge_type = 'posted')
+            G.add_edge(job_data['company_id'], job_id, weight = 1, 
+                        edge_type = 'posted')
+
+        # add candidates to the network. Candidate node ids are set and managed
+        # by the network
+
+        for _, candidate_data in self.cv_data.items():
+            candidate_id = 'candidate-%d'%G.graph['num_candidates']
+            G.add_node(candidate_id, node_type = 'candidate', **candidate_data)
+            G.graph['num_candidates'] += 1
+        
+        return G
+
+    def add_relations_edges(self) -> None:
+        """This method use Latent semantic analysis to compare different types
+        of nodes to infer the 'similar' relation between them and add those edges
+        to the network
+        """
+
+
